3b56d94ba247b63c1be637b30e37529cd99b1bd9
dk04123 <139839385+dk04123@users.noreply.github.com>
2024-12-29 23:16:38 +0700

    Update CollectionExample1.java
    fds132

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -21,7 +21,7 @@ public class CollectionExample1 {
         arrayList.add("PHP11");
         arrayList.add("Python1");
         arrayList.add("Java1");
-        arrayList.add("C++");
+        arrayList.add("C++")123;
         System.out.println("Các phần tử của ArrayList");
         System.out.print("\t" + arrayList + "\n");
  
