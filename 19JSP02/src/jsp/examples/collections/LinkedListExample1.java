561cb05a36216f2a0edb932a91bda62e8290a823
dk04123 <139839385+dk04123@users.noreply.github.com>
2024-12-30 09:15:26 +0700

    Update LinkedListExample1.java
    gf123

--- 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
+++ 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
@@ -16,7 +16,7 @@ public class LinkedListExample1 {
         LinkedList<String> linkedList = new LinkedList<String>();
         // Add objects to linkedList
         linkedList.add("Java11");
-        linkedList.add("C++");
+        linkedList.add("C++1243");
         linkedList.add("PHP");
         linkedList.add("Java");
         // Show list through Iterator
