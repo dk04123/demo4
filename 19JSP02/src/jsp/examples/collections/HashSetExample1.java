5c1285e0e1e1400b5a6510e7f16c4fe24c97fe53
dk04123 <139839385+dk04123@users.noreply.github.com>
2024-12-31 10:23:16 +0700

    Update HashSetExample1.java
    f213

--- 19JSP02/src/jsp/examples/collections/HashSetExample1.java
+++ 19JSP02/src/jsp/examples/collections/HashSetExample1.java
@@ -18,7 +18,7 @@ public class HashSetExample1 {
         // Add objects to hashset
         set.add("Java22211");
         set.add("C++");
-        set.add("PHP");123
+        set.add("PH2143P");123
         set.add("Java11");
         // Show set through Iterator
         Iterator<String> itr = set.iterator();
