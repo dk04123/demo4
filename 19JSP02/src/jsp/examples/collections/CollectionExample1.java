10c2d0f45382fbc4cab5564b0476ed4e4e7e8c7b
dk04123 <139839385+dk04123@users.noreply.github.com>
2025-01-18 14:25:30 +0700

    Update CollectionExample1.java
    dsa2

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -21,7 +21,7 @@ public class CollectionExample1 {
         arrayList.add("PHP11");
         arrayList.add("Python");
         arrayList.add("Java1");
-        arrayList.add("C++")123;
+        arrayList.add("C++13")123;
         System.out.println("Các phần tử của ArrayList");
         System.out.print("\tgg" + arrayList + "\n");
  
